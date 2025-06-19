0ed9c98dd3063cf4cbdd34642d1114f83af1cc26
Pablo del Barrio <pablodbb@gmail.com>
2025-06-19 12:30:08 +0200

    search movies optimized with useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/';
 
@@ -20,6 +21,9 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('')
   const [movieList, setMovieList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true)
@@ -55,7 +59,7 @@ const App = () => {
 
   useEffect(() => {
     fetchMovies(searchTerm)
-  }, [searchTerm])
+  }, [debouncedSearchTerm])
 
   return (
     <main>
